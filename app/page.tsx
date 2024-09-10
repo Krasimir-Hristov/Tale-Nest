bfe99cfe1e6c5047ea1cbbe617ce6d2a0493fe87
krasi <krasimir.xristov@gmail.com>
2024-09-10 18:02:27 +0200

    Refactor SVG icons and update Tailwind config

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './globals.css'; // Импортирай стила в същата директория
+
+const Home = () => {
+  return <div className='text-red-950'>home</div>;
+};
+
+export default Home;
