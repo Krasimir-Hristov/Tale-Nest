cca637af3a008dcf5890a55a13d3b9d3f3f4105a
krasi <krasimir.xristov@gmail.com>
2024-09-10 22:16:44 +0200

    Refactor Home component to include section with categories, posts, and load more

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './globals.css'; // Импортирай стила в същата директория
 
 const Home = () => {
-  return <div className='text-red-950'>home</div>;
+  return (
+    <section className='flex-start flex-col paddings mb-16 font-bold '>
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>LoadMore</h1>
+    </section>
+  );
 };
 
 export default Home;
