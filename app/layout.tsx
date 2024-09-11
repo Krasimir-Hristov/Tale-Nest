bd0428fb4e48da6bd7968c0b504067ecac5e41fc
krasi <krasimir.xristov@gmail.com>
2024-09-11 18:50:51 +0200

    Refactor layout component to include navbar and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from '@/components/Navbar';
 import './globals.css';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'Tale Nest',
@@ -14,9 +16,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        Navbar
+        <Navbar />
         <main>{children}</main>
-        Footer
+        <Footer />
       </body>
     </html>
   );
