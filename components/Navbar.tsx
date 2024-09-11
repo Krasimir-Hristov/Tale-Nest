1eb45947af6dbb100ec67cb292cdfcf38f66b0e4
krasi <krasimir.xristov@gmail.com>
2024-09-11 19:00:38 +0200

    add a constants folder with some arrays i need latter

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image src='/logo.svg' width={115} height={43} alt='Tale Nest' />
+        </Link>
+        <ul className='xl:flex hidden text-sm gap-7'></ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
